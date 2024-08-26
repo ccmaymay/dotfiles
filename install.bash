a9389ea8ee52057d20745558288359e17a99dc2f
Chandler May <chandler.camille.may@gmail.com>
2024-08-26 10:52:50 -0400

    Add install script.

---
+++ install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+DOTFILES_SRC="$(dirname $0)"
+DOTFILES_DST="$HOME"
+
+echo "Will copy dotfiles from $DOTFILES_SRC to $DOTFILES_DST"
+echo "Proceed? (y/n)"
+echo -n "> "
+
+read answer
+
+if [[ "${answer,}" == y ]]
+then
+        rm -rf "$DOTFILES_DST/.ipython"
+        cp -r "$DOTFILES_SRC"/.[^.]* "$DOTFILES_DST"/
+        echo "Done."
+else
+        echo "Aborting." >&2
+        exit 1
+fi
